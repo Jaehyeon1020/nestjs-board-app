b53c2f1895896710673a460658c9b21dcd2c336a
Jaehyeon1020 <kjhkk1020@naver.com>
2023-01-29 21:40:33 +0900

    feat: 인증 모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { JwtStratege } from './jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
@@ -18,6 +19,7 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStratege], // 서비스들을 이 모듈에서 사용할 수 있도록 등록
+  exports: [JwtStratege, PassportModule], // 서비스들을 다른 모듈에서도 사용할 수 있도록 등록
 })
 export class AuthModule {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board, BoardRepository])],
+  imports: [TypeOrmModule.forFeature([Board, BoardRepository]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository],
 })
