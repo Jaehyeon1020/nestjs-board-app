f0ca1002da9140daab3a9182b7c1ce1ac2eea3ab
Jaehyeon1020 <kjhkk1020@naver.com>
2023-01-29 13:39:08 +0900

    feat: 입력 데이터 유효성 검증 구현

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: '비밀번호에 영어 알파벳과 숫자만 사용이 가능합니다.',
+  }) // 영어, 숫자만 사용 가능
   password: string;
 }
