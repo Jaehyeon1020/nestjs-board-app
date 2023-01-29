7d61984b6ecd68feccb1e608cf9bb3888c86cd4e
Jaehyeon1020 <kjhkk1020@naver.com>
2023-01-29 21:41:28 +0900

    feat: 인증 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +27,11 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.login(authCredentialDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard()) // req 객체에 토큰에 담긴 User 정보를 저장하도록 하고, 토큰의 유효성 검사를 함
+  test(@GetUser() user: User) {
+    console.log('user', user);
   }
 }
